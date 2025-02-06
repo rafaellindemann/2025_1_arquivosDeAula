57f3f9e6272199e5376aa2dc14dd0ca3ba1da178
rafaellindemann <rafaelld@gmail.com>
2025-02-06 16:05:59 -0300

    Add parcial revisao v3

---
+++ 3a/v2/revizona/src/components/Body.jsx
@@ -0,0 +1,11 @@
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='container-body'>
+        <h1>Empresa de logos</h1>
+    </div>
+  )
+}
+
+export default Body
---
+++ 3a/v2/revizona/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+function Header() {
+  return (
+    <div className="container-header">
+        <p>LoGo</p>
+    </div>
+  )
+}
+
+export default Header
