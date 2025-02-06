ad3ddc33d2cdbd5fcb8c818c0b5be2eb8a2ca077
rafaellindemann <rafaelld@gmail.com>
2025-02-06 17:16:52 -0300

    add primeira parte rev 3aV2

--- 3a/v2/revizona/src/components/Demo1.jsx
+++ 3a/v2/revizona/src/components/Demo1.jsx
@@ -1,8 +1,34 @@
+import { useState } from 'react';
 import './Demo1.css'
 function Demo1() {
+    const [estado, setEstado] = useState(0)
+
+    function incrementarEstado(){
+        setEstado(estado + 1)
+    }
+
+    let cornometro = 9;
+    function incrementaCornometro(){
+        cornometro++
+        console.log(cornometro);
+    }
+    function alertarUsuario(){
+        alert("Corra para as colinas!!")
+    }
   return (
     <div className="container-demo1">
-        Demo1
+        <h2>Demo1</h2>
+        <button onClick={alertarUsuario}>Clica em mim</button>
+        <button onClick={() => {alert("Executei uma arrow function")}}>Ex Arrow Function</button>
+
+        <div>
+            Cornometro: {cornometro}
+            <button onClick={incrementaCornometro}>+</button>
+        </div>
+        <div>
+            estado: {estado}
+            <button onClick={incrementarEstado}>+</button>
+        </div>
     </div>
   )
 }
