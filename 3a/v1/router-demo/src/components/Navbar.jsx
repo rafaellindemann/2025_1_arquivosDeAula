23b927f89eeea60fbfa1fe56469d7807bc3fe042
rafaellindemann <rafaelld@gmail.com>
2025-02-27 17:16:09 -0300

    add demo router v1 com cards

--- 3a/v1/router-demo/src/components/Navbar.jsx
+++ 3a/v1/router-demo/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className="container-navbar">
-        <Link to="/">Home</Link>
+        <Link className="p" to="/">Home</Link>
         <Link to="/produtos">Produtos</Link>
         <Link to="/avisos">Avisos</Link>
         <Link to="/contato">Contato</Link>
