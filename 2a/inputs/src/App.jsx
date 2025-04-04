ad9c6bd0deb10e6fd3857552569eeddfe642c08b
rafaellindemann <rafaelld@gmail.com>
2025-04-03 21:07:16 -0300

    add inputs controlados 2aN1

---
+++ 2a/inputs/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const[inputUsername, setInputUsername] = useState('')
+  const[inputSenha, setInputSenha] = useState('')
+
+  function efetuarLogin(){
+    alert(inputUsername)
+  }
+
+  return (
+    <div className='container-app'>
+      <h1>Inputs controlados</h1>
+      <label htmlFor="">Username</label>
+      <input type="text" 
+        value={inputUsername}
+        onChange={(event) => setInputUsername(event.target.value)}  
+      />
+
+      <label htmlFor="">Senha</label>
+      <input type="text" 
+        value={inputSenha}
+        onChange={(event) => setInputSenha(event.target.value)}
+      />
+      <button onClick={efetuarLogin}>Login</button>
+    </div>
+  )
+}
+
+export default App
