192b4ff64a334ffa5f5aba7170474c1eca7f0141
rafaellindemann <rafaelld@gmail.com>
2025-04-17 21:00:31 -0300

    add calculadera de botao

--- 1a/js/00-primeiro-js/script.js
+++ 1a/js/00-primeiro-js/script.js
@@ -1,3 +1,4 @@
 function tratarBotao(){
     alert("Clicou muito bem!")
 }
+
---
+++ 1a/js/01-exercicios/exercicios.js
@@ -0,0 +1,21 @@
+
+function somar(){
+    let n1, n2, soma
+
+    n1 = Number(prompt("Primeiro número: "))
+    // n2 = prompt("Segundo número: ")
+    // n2 = Number(n2)
+    n2 = Number(prompt("Segundo número: "))
+
+    soma = n1 + n2
+
+    alert("Soma: " + soma)
+}
+
+function diminuir(){
+    let n1, n2, diferenca
+    n1 = Number(prompt("Primeiro número: "))
+    n2 = Number(prompt("Segundo número: "))
+    diferenca = n1 - n2
+    alert("Diferença: " + diferenca)
+}
