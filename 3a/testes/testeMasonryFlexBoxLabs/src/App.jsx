1f51be8ad794d00c7e82c6d56f60e9c6eeffb7a8
rafaellindemann <rafaelld@gmail.com>
2025-04-23 18:16:28 -0300

    add teste masonry com flex

---
+++ 3a/testes/testeMasonryFlexBoxLabs/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="container">
+        <div class="item">1</div>
+        <div class="item">2</div>
+        <div class="item">3</div>
+        <div class="item">4</div>
+        <div class="item">5</div>
+        <div class="item">6</div>
+        <div class="item">7</div>
+        <div class="item">8</div>
+        <div class="item">9</div>
+      </div>
+    </>
+  )
+}
+
+export default App
