4ae7d8293e191d99b5f04a7831ffd3277b0884f1
rafaellindemann <rafaelld@gmail.com>
2025-04-30 20:28:53 -0300

    add wip farmarcia

---
+++ 2a/10-farmarcia/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+import Logo from './Logo'
+
+function Header() {
+  return (
+    <div className='container-header'>
+        <Logo />
+        <h1>Farmárcia</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ 2a/10-farmarcia/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import './Logo.css'
+
+function Logo() {
+  return (
+    <img src="/img/farmarcia-logo-pq.PNG" alt="logo da farmárcia" className="img-logo"/>
+  )
+}
+
+export default Logo
