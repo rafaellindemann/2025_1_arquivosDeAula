c589c0c69e809cc7073d49308364051db478163c
rafaellindemann <rafaelld@gmail.com>
2025-05-14 20:16:13 -0300

    add exemplo de pages no 00-

--- 2a/00-helloReactFofo/src/App.jsx
+++ 2a/00-helloReactFofo/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Corpo from './components/Corpo'
 import Navbar from './components/Navbar'
+import Cadastro from './pages/Cadastro'
+import Login from './pages/Login'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Corpo />
+      {/* <Login /> */}
+      <Cadastro />
+      {/* <Navbar />
+      <Corpo /> */}
  
     </>
   )
---
+++ 2a/00-helloReactFofo/src/pages/Cadastro.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Cadastro() {
+  return (
+    <div>
+        <h1>Cadastro</h1>
+        <div className="inputContainer">
+            <label htmlFor="">Nome</label>
+            <input type="text" />
+        </div>
+        <div className="inputContainer">
+            <label htmlFor="">E-mail</label>
+            <input type="text" />
+        </div>
+        <button>CADASTRAR AÍ</button>
+    </div>
+
+  )
+}
+
+export default Cadastro
