40de5917d8c8d86d6561beffdf0be0d98bf7e94e
rafaellindemann <rafaelld@edu.sc.senai.br>
2025-05-22 22:27:32 -0300

    add começo login/cadastro

---
+++ 1a/js/03-telas/script.js
@@ -0,0 +1,9 @@
+
+function mostrarLogin(){
+    document.getElementById('login').style.display = 'flex'
+    document.getElementById('cadastro').style.display = 'none'
+}
+function mostrarCadastro(){
+    document.getElementById('login').style.display = 'none'
+    document.getElementById('cadastro').style.display = 'flex'
+}
