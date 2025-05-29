55e8f26dcbfa0b4c8ab5a6a1e9bc42b13ca9db4c
rafaellindemann <rafaelld@gmail.com>
2025-05-28 22:03:29 -0300

    att context e login

--- 2a/14-router-context/src/pages/Home.jsx
+++ 2a/14-router-context/src/pages/Home.jsx
@@ -6,25 +6,25 @@ function Home() {
     const {usuarioLogado, setUsuarioLogado, idadeUsuario} = useContext(GlobalContext)
     const navigate = useNavigate(); // Hook para navegação 
 
-    function logar(){
-      // processo de validação do usuário
-      let nome = prompt("Username: ")
-      let senha = prompt("Senha: ")
-      if(senha == '1235'){
-        setUsuarioLogado(nome)
-        navigate('/final');
-      }else{
-        alert("Erro 403")
-      }
-    }
+    // function logar(){
+    //   // processo de validação do usuário
+    //   let nome = prompt("Username: ")
+    //   let senha = prompt("Senha: ")
+    //   if(senha == '1235'){
+    //     setUsuarioLogado(nome)
+    //     navigate('/final');
+    //   }else{
+    //     alert("Erro 403")
+    //   }
+    // }
 
   return (
     <div>
         <Navbar />
       <h1>Página home do site</h1>
-      <p>Olá {usuarioLogado}, {idadeUsuario} anos</p>
+      <p>Olá {usuarioLogado.username}, {idadeUsuario} anos</p>
 
-      <button onClick={logar}>Login</button>
+      {/* <button onClick={logar}>Login</button> */}
 
     </div>
   )
--- 2a/14-router-context/src/pages/Login.jsx
+++ 2a/14-router-context/src/pages/Login.jsx
@@ -4,13 +4,14 @@ import { useNavigate } from 'react-router-dom'
 function Login() {
     const[inputUsername, setInputUsername] = useState('')
     const[inputSenha, setInputSenha] = useState('')
-    const{usuarios} = useContext(GlobalContext)
+    const{usuarios, setUsuarioLogado} = useContext(GlobalContext)
     const navigate = useNavigate()
 
     function efetuarLogin(){
       const usuario = usuarios.find((u) => u.username == inputUsername && u.senha == inputSenha)
       if(usuario){
         alert("Login efetuado com sucesso")
+        setUsuarioLogado(usuario)
         navigate('/home')
       }else{
         alert("Credenciais incorretas")
