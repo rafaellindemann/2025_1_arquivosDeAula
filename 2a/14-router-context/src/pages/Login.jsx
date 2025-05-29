535ee54a80fede17c85dc3d4d84a0780f58eabec
rafaellindemann <rafaelld@gmail.com>
2025-05-28 21:22:19 -0300

    att context

--- 2a/14-router-context/src/pages/Login.jsx
+++ 2a/14-router-context/src/pages/Login.jsx
@@ -6,10 +6,16 @@ function Login() {
     <div>
         <h1>Login</h1>
         <div>
-            username <input type="text" />
+            username <input type="text" 
+              value={inputUsername}
+              onChange={(e) => setInputUsername(e.target.value)}
+              /> 
         </div>
         <div>
-            Senha <input type="text" />
+            Senha <input type="text" 
+              value={inputSenha}
+              onChange={(e) => setInputSenha(e.target.value)}
+            />
         </div>
         <button>Login</button>
     </div>
