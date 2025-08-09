e6ba23b28f70a2c7b6caaeda7cedaed4503805c5
rafaellindemann <rafaelld@gmail.com>
2025-08-08 21:17:21 -0300

    add wip temperaturas

---
+++ cursoDeSexta/03-arrays/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [min, setMin] = useState([])
+  const [max, setMax] = useState([])
+
+  function gerar(){
+    let tempMin = min
+    let tempMax = max
+    for(let i=0; i<30; i++){
+
+      let n1 = Math.floor(Math.random() * 24 + 12)
+      let n2 = Math.floor(Math.random() * 24 + 12)
+  
+      if(n1 > n2){
+        tempMax.push(n1)
+        tempMin.push(n2)
+      }else{
+        tempMax.push(n2)
+        tempMin.push(n1)
+      }
+  
+    }
+    console.table(tempMin)
+    console.table(tempMax)
+    setMax(tempMax)
+    setMin(tempMin)
+  }
+  
+  return (
+    <>
+      <button onClick={gerar}>gerar número</button>
+      
+    </>
+  )
+}
+
+export default App
+
+// sempre arredonda pra baixo {0..9}
+// Math.floor() 
+
+// sempre arrendonda pra cima {1..10}
+// Math.ceil()
+
+// arredonda pro inteiro mais próximo {0..10}
+// Math.round()
